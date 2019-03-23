fe5ece5b72b8920d0e1bf4fc949303218cb6d415
Dragan Filipovic <dragan.filipovic@netcentric.biz>
2019-03-23 03:37:22 +0100

    Update api

--- config.js
+++ config.js
@@ -1,7 +1,6 @@
 const config = {
     domain: 'http://henkel-author-qa.nhe.netcentric.biz:4502', //'http://localhost:4502', //'http://192.168.178.37:4502',
     pagePath: 'content/test/oneweb/master/en/ml',
-    api: 'http://localhost:8080/mock', //'https://localhost:4567'//,
     auth: {
         user: 'admin',
         pass: 'admin'
--- public/js/init.js
+++ public/js/init.js
@@ -1,7 +1,8 @@
 const config = {
     domain: 'http://henkel-author-qa.nhe.netcentric.biz:4502',//'http://localhost:4502', //'http://192.168.178.37:4502',
     pagePath: 'content/test/oneweb/master/en/ml',
-    api: 'http://localhost:8080/mock' //'https://localhost:4567'//
+    api: 'https://localhost:4567' // 'http://localhost:8080/mock', //
 };
+
 window.NC = config;
 console.log(NC);
